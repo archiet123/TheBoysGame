8e0e08870c99145bfe3026646a03739abe7a9086
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-06-18 23:22:57 +0100

    created script

---
+++ Assets/Scripts/InventorySys/InventorySystem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public GameObject Slot1;
+    public GameObject Slot2;
+    public GameObject Slot3;
+
+    //for a start literally just do, 
+    //when button clicked in ui, add image to a "slot"
+    //
+}
