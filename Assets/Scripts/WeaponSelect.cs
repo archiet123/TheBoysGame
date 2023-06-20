d98ba2bc8f184561a5f7620d52de1a2bbfb954a7
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-06-20 10:12:05 +0100

    ui will not display

--- Assets/Scripts/WeaponSelect.cs
+++ Assets/Scripts/WeaponSelect.cs
@@ -5,20 +5,21 @@ using UnityEngine.UI;
 
 public class WeaponSelect : MonoBehaviour
 {
+
     public WeaponChoice[] weaponChoices;
     public GameObject ButtonParent;
 
-    // private void OnEnable()
-    // {
-    //     for (int i = 0; i < GameManager.Instance.weaponCounts[GameManager.Instance.currentWeapon]; i++)
-    //     {
-    //         int WeaponNum = i + 1;
-    //         weaponChoices[i].gameObject.SetActive(true);
-    //         weaponChoices[i].WeaponText.text = (i + 1).ToString();
-    //         weaponChoices[i].GetComponent<Button>().onClick.RemoveAllListeners();
-    //         weaponChoices[i].GetComponent<Button>().onClick.AddListener(() => SelectWeapon(GameManager.Instance.currentWeapon, WeaponNum));
-    //     }
-    // }
+    private void OnEnable()
+    {
+        for (int i = 0; i < GameManager.Instance.weaponCounts[GameManager.Instance.currentWeapon]; i++)
+        {
+            int WeaponNum = i + 1;
+            weaponChoices[i].gameObject.SetActive(true);
+            weaponChoices[i].WeaponText.text = (i + 1).ToString();
+            weaponChoices[i].GetComponent<Button>().onClick.RemoveAllListeners();
+            weaponChoices[i].GetComponent<Button>().onClick.AddListener(() => SelectWeapon(GameManager.Instance.currentWeapon, WeaponNum));
+        }
+    }
 
     private void SelectWeapon(int Weapon, int ID)
     {
