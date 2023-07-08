1c53b67c01c3b075b50ffecb12b41e5a43143374
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-07-08 22:58:38 +0100

    no ui errors

--- Assets/Scripts/GameScripts/DisplayOptions.cs
+++ Assets/Scripts/GameScripts/DisplayOptions.cs
@@ -1,11 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using System.Collections;
 
 public class DisplayOptions : MonoBehaviour
 {
     public GameObject PauseMenu;
     public GameObject OptionsMenu;
+    public Button BackButton;
+    public GameObject player;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            // BackButton.onClick.Invoke();
+            // var eventSystem = EventSystem.current;//setting event system
+            // ExecuteEvents.Execute(BackButton.gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
+            HideOptions();
+        }
+    }
 
     public void ShowOptions()
     {
@@ -17,5 +31,9 @@ public class DisplayOptions : MonoBehaviour
     {
         OptionsMenu.SetActive(false);
         PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        player.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
