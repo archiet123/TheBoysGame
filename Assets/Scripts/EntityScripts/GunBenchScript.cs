ec6df9eb5c8f71d2ff001d20fc0f9274769bfe34
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-07-09 13:43:49 +0100

    added comments

--- Assets/Scripts/EntityScripts/GunBenchScript.cs
+++ Assets/Scripts/EntityScripts/GunBenchScript.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunBenchScript : UIController
+public class GunBenchScript : UIController, IInteractable
 {
-
-
+    //this script is to be the gun bench interactable
 }
--- Assets/Scripts/EntityScripts/UIController.cs
+++ Assets/Scripts/EntityScripts/UIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour, IInteractable
+public class UIController : MonoBehaviour
 {
     //UI   
     public GameObject pauseMenu;
