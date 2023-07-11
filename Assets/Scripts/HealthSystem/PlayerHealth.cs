6de21000b07d4b36f092e6e81b2ca9ec5ffd8ec5
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-07-11 14:21:26 +0100

    working player health slider2

--- Assets/Scripts/HealthSystem/PlayerHealth.cs
+++ Assets/Scripts/HealthSystem/PlayerHealth.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    //variables
     public int Health;
+
+    //GameObjects
     public GameObject GameManager;
 
+    //UI Objects
+    public Slider HealthSlider;
 
     void Update()
     {
+
         if (Health <= 0)
         {
             Debug.Log("you lose");
@@ -17,10 +24,16 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void SetHealth(int CurrentHealth)
+    {
+        Debug.Log("sethealth");
+        HealthSlider.value = CurrentHealth;
+    }
+
     public void DealDamage(int EnemyDamage)
     {
         // Debug.Log("test");
         Health = Health - EnemyDamage;
-
+        SetHealth(Health);
     }
 }
